9dae295145bfe5fe9a27a6d0bb7c81f3c59ba679
mamun07 <mamunben7@gmail.com>
2022-02-20 19:22:16 +0600

    mui install complete

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Typography } from '@mui/material'
 
 export default function Home() {
   return (
@@ -12,23 +12,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Typography variant='h2'> Welcome to Next.js!</Typography>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
