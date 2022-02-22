f4e522da720898d527ecbf24d59088c0db9e317c
mamun07 <mamunben7@gmail.com>
2022-02-22 19:24:39 +0600

    logo add

--- frontend/pages/container/Header.jsx
+++ frontend/pages/container/Header.jsx
@@ -1,13 +1,14 @@
-import { Box, Container, Grid, Typography } from '@mui/material'
 import React from 'react'
+import Image from 'next/image'
+import { Box, Container, Grid, Typography } from '@mui/material'
 
 export default function Header() {
   return (
-    <Box sx={{backgroundColor: 'primary.none'}}>
+    <Box position='sticky' py={3} sx={{backgroundColor: 'primary.none'}}>
       <Container maxWidth="xl">
-        <Grid container spacing={2}>
+        <Grid container spacing={0}>
           <Grid item xs={2}>
-            <Typography>Logo</Typography>
+            <Image src='./images/logo.svg' alt="website logo"/>
           </Grid>
           <Grid item xs={8}>
             <Typography>Menu</Typography>
